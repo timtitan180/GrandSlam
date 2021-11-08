edf7989634bf1f2dd58354d5e0bc507ce9432f4c
Tim Tudosa <timtudosa6@gmail.com>
2021-11-08 10:25:57 -0600

    Cleaned up sign up controller and added role that gets added to database on input

--- src/main/java/com/application/grandslam/controller/SignUpController.java
+++ src/main/java/com/application/grandslam/controller/SignUpController.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import com.application.grandslam.database.entities.Role;
 import com.application.grandslam.database.entities.User;
-import com.application.grandslam.database.entities.UserRole;
 import com.application.grandslam.database.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -21,18 +23,15 @@ import com.application.grandslam.forms.SignUpForm;
 
 
 
-@RestController
+@Controller
 public class SignUpController {
+	public static final Logger LOG = LoggerFactory.getLogger(SignUpController.class);
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
 	@Autowired
 	UserService userService;
-
-	/*
-	 * @Autowired PasswordEncoder passwordEncoder;
-	 */
 
 	@GetMapping(value = "signup")
 	public ModelAndView GetLoginUser() {
@@ -42,11 +41,10 @@ public class SignUpController {
 
 	}
 
-	@PostMapping(value = "signup")
-	public ModelAndView PostLoginUser(HttpServletResponse response, @Valid @ModelAttribute("SignUpForm") SignUpForm form, BindingResult bindingResult) {
+	@PostMapping(value = "players")
+	public ModelAndView PostLoginUser(HttpSession session, HttpServletResponse response, @Valid @ModelAttribute("SignUpForm") SignUpForm form, BindingResult bindingResult) {
 		ModelAndView postSignupPage = new ModelAndView("signup");
 		postSignupPage.addObject("form", form);
-
 		List<String> errors = new ArrayList<String>();
 
 		for (FieldError error : bindingResult.getFieldErrors()) {
@@ -60,61 +58,26 @@ public class SignUpController {
 		if (errors.size() > 0) {
 			return postSignupPage;
 		}
-
-		else {
-			List<User> users = new ArrayList<User>();
-			List<UserRole> userRoles = new ArrayList<UserRole>();
 			User user = new User();
-			UserRole userRole = new UserRole();
+			Role role = new Role();
+			postSignupPage.addObject("role",role);
 			user.setFirstName(form.getFirstName());
 			user.setLastName(form.getLastName());
 			user.setEmail(form.getEmail());
-			userRole.setRole("USER");
+			role.setRole(form.getRole());
+			user.setRole(role);
 			user.setPassword(passwordEncoder.encode(form.getPassword()));
 
 			userService.save(user);
-			System.out.println("User has been saved...");
-			return new ModelAndView("showPlayers");
+			session.setAttribute("user",user.getRole());
+			LOG.info(user.toString());
+			LOG.info(role.toString());
+			System.out.println(role.getRole().toString());
 			
-		}
 
+		return new ModelAndView("showPlayers");
 	}
 
 
 }
 
-// need to get fullname,email and password from the form in the login jsp
-// I need to create a session and get the attributes of name email and password
-// from the getters of the Users bean
-//		request.getAttribute("name");
-//		request.getAttribute("password");
-
-//	@RequestMapping(value = "/login", method = RequestMethod.GET)
-//	public ModelAndView getLogin(HttpServletRequest request, HttpServletResponse response) {
-//		ModelAndView result = new ModelAndView("login");
-//		return result;
-//	}
-
-//	@RequestMapping(value = "/login", method = RequestMethod.POST)
-//	public ModelAndView getPostLogin(@RequestParam String name, @RequestParam String email,@RequestParam String password, HttpServletRequest request, HttpServletResponse response,
-//			HttpSession session) {
-//		ModelAndView result = new ModelAndView();
-//		Users user = new Users();
-//		if(name != null) {
-//			result.addObject("name");
-//			result.addObject("password");
-//			result.addObject("email");
-//		}
-//
-//		if(!"tom".equals(session.getAttribute("name")) || !"john".equals(session.getAttribute("password")) || !"ryan@gmail.com".equals("email")){
-//			result.addObject("message","Please enter your information");
-//			return result;
-//		}
-//		else {
-//			result.setViewName("redirect:/success");
-//			result.addObject("name", session.getAttribute("name"));
-//			result.addObject("password", session.getAttribute("password"));
-//
-//		}
-//		return result;
-//	}
--- src/main/java/com/application/grandslam/database/entities/Role.java
+++ src/main/java/com/application/grandslam/database/entities/Role.java
@@ -1,18 +1,2 @@
-package com.application.grandslam.database.entities;
-
-
-
-import javax.persistence.*;
-
-@Entity
-@Table(name="roles")
-class Roles {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="role_id")
-    private Integer roleId;
-
-    @Column(name="role")
-    private String role;
+package com.application.grandslam.database.entities;public class Role {
 }
-
